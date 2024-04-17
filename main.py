4d9abab7d64c9312ea7941783ecb994c49e6b40a
Andrew13X <6594071@gmail.com>
2024-04-17 18:59:30 +0300

    main function fix

--- main.py
+++ main.py
@@ -15,6 +15,4 @@ def serialize_car_object(car: Car) -> bytes:
 def deserialize_car_object(json: bytes) -> Car:
     stream = io.BytesIO(json)
     data = JSONParser().parse(stream)
-    serializer = CarSerializer(data=data)
-    serializer.is_valid(raise_exception=True)
-    return Car.objects.create(**serializer.validated_data)
+    return Car.objects.create(**data)
